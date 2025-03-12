cf4201a69ed3d5b85f3bb2d01e7571bfb02a30d0
Yash parmar <Yash.parmar2908@gmail.com>
2025-03-12 18:01:17 +0530

    Update script.sql

--- script.sql
+++ script.sql
@@ -1,14 +1,12 @@
--- Create the appdb database
+-- Create the appdb database if it does not exist
 CREATE DATABASE IF NOT EXISTS appdb;
 
 -- Use the appdb database
 USE appdb;
 
--- Create the apptb table
-CREATE TABLE `appdb`.`apptb` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`id`));
-
-
-
+-- Create the apptb table only if it does not exist
+CREATE TABLE IF NOT EXISTS apptb (
+  id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(45) NOT NULL,
+  PRIMARY KEY (id)
+);
